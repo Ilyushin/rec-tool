ba05791e0d1d206a9f359b541172947caabe7f02
Olga Alieva <olga.mail.wap@gmail.com>
2020-04-29 11:32:46 +0300

    Change metrics

--- cf_experiments_loop/metrics.py
+++ cf_experiments_loop/metrics.py
@@ -3,3 +3,22 @@ import tensorflow as tf
 
 def accuracy():
     return tf.keras.metrics.Accuracy()
+
+def precision():
+    return tf.keras.metrics.Precision()
+
+def recall():
+    return tf.keras.metrics.Recall()
+
+def roc():
+    return tf.keras.metrics.Accuracy()
+
+def f_score():
+
+    return tf.keras.metrics.Accuracy()
+
+def rmse():
+    return tf.keras.metrics.RootMeanSquaredError()
+
+def mae():
+    return tf.keras.metrics.MeanAbsoluteError()
