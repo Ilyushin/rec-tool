7ca43f558c08345e7cb95afd2deb56f1a9a8d16a
Vladislav Gurin <gurinvlad23@gmail.com>
2020-05-18 20:29:15 +0300

    time run

--- cf_experiments_loop/main.py
+++ cf_experiments_loop/main.py
@@ -2,8 +2,9 @@ import os
 import sys
 import yaml
 import argparse
-import tensorflow as tf
 import pandas as pd
+from time import time
+import tensorflow as tf
 from cf_experiments_loop.common import fn
 from cf_experiments_loop.train_model import train_model
 from cf_experiments_loop.ml_flow.ml_flow import log_to_mlflow
@@ -75,6 +76,8 @@ def main():
                     for e in map(int, epoch):
                         for optimizer in optimizers:
 
+                            start = time()
+
                             history_train, history_eval, test_performance = train_model(
                                 train_data=train_data,
                                 test_data=test_data,
@@ -96,7 +99,8 @@ def main():
                                           group_name=str(model),
                                           params={'batch_size': batch,
                                                   'epoch': e,
-                                                  'optimizer': str(optimizer)},
+                                                  'optimizer': str(optimizer),
+                                                  'run_time': time() - start},
                                           metrics={'metrics': test_performance},
                                           tags={'dataset': 'movielens'},
                                           artifacts=[model_dir, results_csv])
@@ -113,6 +117,8 @@ def main():
 
         else:
 
+            start = time()
+
             history_train, history_eval, test_performance = train_model(
                 train_data=train_data,
                 test_data=test_data,
@@ -128,6 +134,17 @@ def main():
                 epoch=epoch,
                 optimizer=tf.keras.optimizers.Adam()
             )
+
+            # write to MLFlow
+            log_to_mlflow(project_name='Recommendations',
+                          group_name=str(model_fn),
+                          params={'batch_size': batch_size,
+                                  'epoch': epoch,
+                                  'optimizer': 'Adam',
+                                  'run_time': time() - start},
+                          metrics={'metrics': test_performance},
+                          tags={'dataset': 'movielens'},
+                          artifacts=[model_dir, results_csv])
 
             # write to csv file
             pd.DataFrame({
