26cbc5b977792fd8cdae43916067b646d00b622e
Eugene Ilyushin <eugene.ilyushin@gmail.com>
2020-05-19 15:29:49 +0300

    Fixed name of dataset

--- cf_experiments_loop/main.py
+++ cf_experiments_loop/main.py
@@ -128,12 +128,12 @@ def main():
                                 df_results = pd.DataFrame(
                                     get_result(model_conf['metrics'], history_eval, model_path, batch,
                                                e, optimizer,
-                                               [input_data_conf['movielens']['type']], as_list=True)
+                                               input_data_conf['movielens']['type'], as_list=True)
                                 )
                             else:
                                 df_results = df_results.append(
                                     get_result(model_conf['metrics'], history_eval, model_path, batch,
-                                               e, optimizer, [input_data_conf['movielens']['type']]),
+                                               e, optimizer, input_data_conf['movielens']['type']),
                                     ignore_index=True)
 
                             df_results.to_csv(results_csv)
