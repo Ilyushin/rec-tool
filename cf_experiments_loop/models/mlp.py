dcd06602e20bb23bfbf2e706d65c405832340969
Vladislav Gurin <gurinvlad23@gmail.com>
2020-05-24 22:19:04 +0300

    mlp model fixing

--- cf_experiments_loop/models/mlp.py
+++ cf_experiments_loop/models/mlp.py
@@ -1,6 +1,3 @@
-from keras.models import Model
-from keras.layers import Embedding, Flatten, Input, merge, Dropout, Dense, BatchNormalization
-from keras.optimizers import Adam
 import tensorflow as tf
 
 
@@ -8,40 +5,40 @@ def mlp(users_number: int, items_number: int):
     latent_dim = 10
 
     # Define inputs
-    movie_input = Input(shape=[1],name='movie-input')
-    user_input = Input(shape=[1], name='user-input')
+    movie_input = tf.keras.layers.Input(shape=[1], name='movie-input')
+    user_input = tf.keras.layers.Input(shape=[1], name='user-input')
 
     # MLP Embeddings
-    movie_embedding_mlp = Embedding(items_number + 1, latent_dim, name='movie-embedding-mlp')(movie_input)
-    movie_vec_mlp = Flatten(name='flatten-movie-mlp')(movie_embedding_mlp)
+    movie_embedding_mlp = tf.keras.layers.Embedding(items_number + 1, latent_dim, name='movie-embedding-mlp')(movie_input)
+    movie_vec_mlp = tf.keras.layers.Flatten(name='flatten-movie-mlp')(movie_embedding_mlp)
 
-    user_embedding_mlp = Embedding(users_number + 1, latent_dim, name='user-embedding-mlp')(user_input)
-    user_vec_mlp = Flatten(name='flatten-user-mlp')(user_embedding_mlp)
+    user_embedding_mlp = tf.keras.layers.Embedding(users_number + 1, latent_dim, name='user-embedding-mlp')(user_input)
+    user_vec_mlp = tf.keras.layers.Flatten(name='flatten-user-mlp')(user_embedding_mlp)
 
     # MF Embeddings
-    movie_embedding_mf = Embedding(items_number + 1, latent_dim, name='movie-embedding-mf')(movie_input)
-    movie_vec_mf = Flatten(name='flatten-movie-mf')(movie_embedding_mf)
+    movie_embedding_mf = tf.keras.layers.Embedding(items_number + 1, latent_dim, name='movie-embedding-mf')(movie_input)
+    movie_vec_mf = tf.keras.layers.Flatten(name='flatten-movie-mf')(movie_embedding_mf)
 
-    user_embedding_mf = Embedding(users_number + 1, latent_dim, name='user-embedding-mf')(user_input)
-    user_vec_mf = Flatten(name='flatten-user-mf')(user_embedding_mf)
+    user_embedding_mf = tf.keras.layers.Embedding(users_number + 1, latent_dim, name='user-embedding-mf')(user_input)
+    user_vec_mf = tf.keras.layers.Flatten(name='flatten-user-mf')(user_embedding_mf)
 
     # MLP layers
-    concat = tf.keras.layers.concatenate([movie_vec_mlp, user_vec_mlp], mode='concat', name='concat')
-    concat_dropout = Dropout(0.2)(concat)
-    fc_1 = Dense(100, name='fc-1', activation='relu')(concat_dropout)
-    fc_1_bn = BatchNormalization(name='batch-norm-1')(fc_1)
-    fc_1_dropout = Dropout(0.2)(fc_1_bn)
-    fc_2 = Dense(50, name='fc-2', activation='relu')(fc_1_dropout)
-    fc_2_bn = BatchNormalization(name='batch-norm-2')(fc_2)
-    fc_2_dropout = Dropout(0.2)(fc_2_bn)
+    concat = tf.keras.layers.Concatenate()([movie_vec_mlp, user_vec_mlp])
+    concat_dropout = tf.keras.layers.Dropout(0.2)(concat)
+    fc_1 = tf.keras.layers.Dense(100, name='fc-1', activation='relu')(concat_dropout)
+    fc_1_bn = tf.keras.layers.BatchNormalization(name='batch-norm-1')(fc_1)
+    fc_1_dropout = tf.keras.layers.Dropout(0.2)(fc_1_bn)
+    fc_2 = tf.keras.layers.Dense(50, name='fc-2', activation='relu')(fc_1_dropout)
+    fc_2_bn = tf.keras.layers.BatchNormalization(name='batch-norm-2')(fc_2)
+    fc_2_dropout = tf.keras.layers.Dropout(0.2)(fc_2_bn)
 
     # Prediction from both layers
-    pred_mlp = Dense(10, name='pred-mlp', activation='relu')(fc_2_dropout)
-    pred_mf = tf.keras.layers.Dot([movie_vec_mf, user_vec_mf], mode='dot', name='pred-mf')
-    combine_mlp_mf = tf.keras.layers.concatenate([pred_mf, pred_mlp], mode='concat', name='combine-mlp-mf')
+    pred_mlp = tf.keras.layers.Dense(10, name='pred-mlp', activation='relu')(fc_2_dropout)
+    pred_mf = tf.keras.layers.Dot(axes=1)([movie_vec_mf, user_vec_mf])
+    combine_mlp_mf = tf.keras.layers.Concatenate()([pred_mf, pred_mlp])
 
     # Final prediction
-    result = Dense(1, name='result', activation='relu')(combine_mlp_mf)
+    result = tf.keras.layers.Dense(1, name='result', activation='relu')(combine_mlp_mf)
 
-    model = Model([user_input, movie_input], result)
+    model = tf.keras.models.Model([user_input, movie_input], result)
     return model
